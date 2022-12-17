5dbba1f21ce71c59a94dd2d62db5630626bde989
Paulo Grolli Grabin <paulograbin@gmail.com>
2022-12-17 15:17:31 -0300

    Change author and log returned data

--- src/app/book-list/books.service.ts
+++ src/app/book-list/books.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { of, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import {map, tap} from 'rxjs/operators';
 import { Book } from './books.model';
 
 @Injectable({ providedIn: 'root' })
@@ -12,8 +12,9 @@ export class GoogleBooksService {
   getBooks(): Observable<Array<Book>> {
     return this.http
       .get<{ items: Book[] }>(
-        'https://www.googleapis.com/books/v1/volumes?maxResults=5&orderBy=relevance&q=oliver%20sacks'
+        'https://www.googleapis.com/books/v1/volumes?maxResults=5&orderBy=relevance&q=neil%20gaiman'
       )
+      .pipe(tap(data => console.log(data)))
       .pipe(map((books) => books.items || []));
   }
 }
