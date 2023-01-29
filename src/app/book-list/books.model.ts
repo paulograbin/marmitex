3bc20c50f8631c6183f82de2d20dcceb6f3d8f01
Paulo Grolli Grabin <paulograbin@gmail.com>
2023-01-29 14:46:44 -0300

    More book attributes

--- src/app/book-list/books.model.ts
+++ src/app/book-list/books.model.ts
@@ -1,7 +1,10 @@
 export interface Book {
   id: string;
+  etag: string;
   volumeInfo: {
     title: string;
+    averageRating: number;
+    pageCount: number;
     authors: Array<string>;
   };
 }
