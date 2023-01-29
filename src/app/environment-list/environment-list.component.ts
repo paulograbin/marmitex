320c2f7e8af53fa6213d96b6b7f55e9ca10257d7
Paulo Grolli Grabin <paulograbin@gmail.com>
2023-01-29 14:46:07 -0300

    Add new component to list environments

---
+++ src/app/environment-list/environment-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-environment-list',
+  templateUrl: './environment-list.component.html',
+  styleUrls: ['./environment-list.component.css']
+})
+export class EnvironmentListComponent {
+
+}
